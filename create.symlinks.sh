68ad56e5f57a3f59beb09aaff095d74267aecaa8
mort1skoda <mort1skoda@gmail.com>
2023-02-09 19:35:42 +0100

    [2023-02-09 17:21:07]

--- create.symlinks.sh
+++ create.symlinks.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
-#
-ln -svf /rep/dotfiles/.config ~
+
+
+
+    ln -svf /rep/dotfiles/.config        ~
+    ln -svf /rep/dotfiles/.vifm          ~
+    ln -svf /rep/dotfiles/.vim           ~
+    ln -svf /rep/dotfiles/.bash_aliases  ~
+    ln -svf /rep/dotfiles/.bash_logout   ~
+    ln -svf /rep/dotfiles/.bashrc        ~
+    ln -svf /rep/dotfiles/.lynxrc        ~
+    ln -svf /rep/dotfiles/.tmux.conf     ~
+    ln -svf /rep/dotfiles/.vimrc         ~
+    ln -svf /rep/dotfiles/.xinitrc       ~
+
+
 
