ed2385f51301c54ea301c04f7ab7861d765f24db
mort1skoda <mort1skoda@gmail.com>
2023-02-17 02:25:00 +0100

    [2023-02-17 02:24:18]

--- suckless/dwm-6.4/config.h
+++ suckless/dwm-6.4/config.h
@@ -29,7 +29,7 @@ static const Rule rules[] = {
   /* class      instance    title       tags mask     isfloating   monitor */
   { "Gimp",     NULL,       NULL,       0,            1,           -1 },
   { "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
-  { "librewolf", NULL,      NULL,       1 << 4,       0,           -1 },
+  { "LibreWolf", NULL,      NULL,       1 << 4,       0,           -1 },
 };
 
 /* layout(s) */
@@ -104,6 +104,7 @@ static const Key keys[] = {
 
   { MODKEY,                       XK_q,      killclient,     {0} },
   { MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+  { MODKEY|ShiftMask,             XK_l,      quit,           {0} },
 };
 
 
