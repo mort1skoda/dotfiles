9d0a3c2dc73e954019d6756f57c6ea1563a42974
mort1skoda <mort1skoda@gmail.com>
2023-03-22 15:13:21 +0100

    [2023-03-22 15:13:21]

--- .bashrc
+++ .bashrc
@@ -36,3 +36,4 @@ export PAGER='less'
 
 #source ~/gruvbox.bash
 
+
---
+++ wm.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#
+echo "wm.sh  --  $0  --  $@"
+
+echo "wm = window manager"
+
+echo "wrapper script"
+
+echo "test"
+
+startx
+
+clear
+echo "back on tty"
+neofetch
+
+
