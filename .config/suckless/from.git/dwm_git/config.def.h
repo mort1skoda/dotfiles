49135ef6f86a1e016735204f6e1788f9d710354a
mort1skoda <mort1skoda@gmail.com>
2023-03-24 22:27:07 +0100

    [2023-03-24 22:27:07]

--- .config/suckless/from.git/dwm_git/config.def.h
+++ .config/suckless/from.git/dwm_git/config.def.h
@@ -1,8 +1,8 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 4;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int snap      = 28;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Source Code Pro Medium:size=10:antialias=true:autohint=true" };
