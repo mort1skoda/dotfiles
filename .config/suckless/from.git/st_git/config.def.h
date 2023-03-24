758d2f98e14c032dd690351ebf666f44d5eb4e20
mort1skoda <mort1skoda@gmail.com>
2023-03-24 22:24:32 +0100

    [2023-03-24 22:24:32]

--- .config/suckless/from.git/st_git/config.def.h
+++ .config/suckless/from.git/st_git/config.def.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Source Code Pro Medium:size=10:antialias=true:autohint=true";
-static int borderpx = 4;
+static int borderpx = 5;
 
 /*
  * What program is execed by st depends of these precedence rules:
