6fdd23f3cebfdfdf4f1ee7655c71d87f5e7c3158
mort1skoda <mort1skoda@gmail.com>
2023-03-24 22:15:10 +0100

    [2023-03-24 22:15:10]

--- .config/suckless/from.git/dwm_git/config.def.h
+++ .config/suckless/from.git/dwm_git/config.def.h
@@ -7,6 +7,7 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
+static const char col_gray0[]       = "#501109";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -14,12 +15,12 @@ static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeNorm] = { col_gray3, col_gray0, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1", "2", "3", "4" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -39,9 +40,9 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "Master/Stack",      tile },    /* first entry is default */
-	{ "><>",               NULL },    /* no layout function means floating behavior */
-	{ "[M]",               monocle },
+	{ "Master/Stack",      tile    },    /* first entry is default */
+	{ "~~Floating~~",      NULL    },    /* no layout function means floating behavior */
+	{ " !MONOCLE!",         monocle },
 };
 
 /* key definitions */
@@ -58,12 +59,14 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *term_ST[]  = { "st", NULL };
+static const char *term_LX[]  = { "lxterminal", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ SUPER,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ SUPER,                       XK_t,      spawn,          {.v = termcmd } },
+	{ SUPER,                       XK_t,      spawn,          {.v = term_ST  } },
+	{ SUPER|ShiftMask,             XK_t,      spawn,          {.v = term_LX  } },
 	{ SUPER,                       XK_b,      togglebar,      {0} },
 	{ SUPER,                       XK_j,      focusstack,     {.i = +1 } },
 	{ SUPER,                       XK_k,      focusstack,     {.i = -1 } },
@@ -104,7 +107,7 @@ static const Button buttons[] = {
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
-	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
+	{ ClkStatusText,        0,              Button2,        spawn,          {.v = term_ST } },
 	{ ClkClientWin,         SUPER,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         SUPER,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         SUPER,         Button3,        resizemouse,    {0} },
