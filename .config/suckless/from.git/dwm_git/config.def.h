e1b9151f9366edf143c9d4f1f2d832320035a547
mort1skoda <mort1skoda@gmail.com>
2023-03-24 22:19:25 +0100

    [2023-03-24 22:19:25]

--- .config/suckless/from.git/dwm_git/config.def.h
+++ .config/suckless/from.git/dwm_git/config.def.h
@@ -5,9 +5,9 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
+static const char *fonts[]          = { "Source Code Pro Medium:size=10:antialias=true:autohint=true" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray0[]       = "#501109";
+static const char col_gray0[]       = "#000000";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
