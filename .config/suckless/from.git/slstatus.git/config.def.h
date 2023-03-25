c3a0e845279fc1406d8d4e07f2d0b9ab9092b849
mort1skoda <mort1skoda@gmail.com>
2023-03-25 13:06:40 +0100

    [2023-03-25 13:06:40]

--- .config/suckless/from.git/slstatus.git/config.def.h
+++ .config/suckless/from.git/slstatus.git/config.def.h
@@ -65,6 +65,6 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{ datetime,     "%s",           "%F %T" },
-    { cpu_freq,     "%s",            NULL   }, 
+	{ datetime,     "  %s  ",           "%F %T" },
+    { cpu_freq,     "  %s  ",            NULL   }, 
 };
