1852cf229bb3bb23a37dd8d0cf3226a15d744d36
mort1skoda <mort1skoda@gmail.com>
2023-03-29 20:21:55 +0200

    [2023-03-29 20:21:54]

--- 02update_git_repos.sh
+++ 02update_git_repos.sh
@@ -9,25 +9,38 @@ echo "running.: $DOTFILES/02update_git_repos.sh"
 # if user is equal to 0 i.e. the user is root: the exit
 [ "$EUID" -eq 0 ] && exit
 
+
+
+
+# init:
+cd $DOTFILES
+DATE=$(date +"[%Y-%m-%d %H:%M:%S]")
 my_user_name="mort1skoda"
 my_user_email="mort1skoda@gmail.com"
-
-cd $DOTFILES
 ORIGIN='main'
 DEFAULT_BRANCH="main"
-
 git config --global user.name  $my_user_name
 git config --global user.email $my_user_email
-git config --global init.$DEFAULT_BRANCH $ORIGIN
+#git config --global init.$DEFAULT_BRANCH $ORIGIN
 #git status
-echo;echo " $DOTFILES :"
-git pull 
-git remote -v | grep --color -i -m1 dotfiles
-git add --all
-git status --short
-DATE=$(date +"[%Y-%m-%d %H:%M:%S]")
-git commit -m "$DATE"
-git push -u origin $ORIGIN
+
+
+
+
+
+
+# here we take care of dotfiles repo:
+echo;echo " $DATA_MOUNT/dotfiles :"
+cd $DATA_MOUNT/dotfiles
+ #git remote -v | grep --color -i -m1 dotfiles
+ git pull 
+ git add --all
+ #git status --short
+ git commit -m "$DATE"
+ git push -u origin $ORIGIN
+cd $DOTFILES
+
+
 
 
 
@@ -35,9 +48,9 @@ git push -u origin $ORIGIN
 # here we take care of devel_C repo:
 echo;echo " $DATA_MOUNT/devel_c:"
 cd $DATA_MOUNT/devel_c
-git pull
-git add --all
-git commit -m "$DATE"
-git push -u origin $ORIGIN
+ git pull
+ git add --all
+ git commit -m "$DATE"
+ git push -u origin $ORIGIN
 cd $DOTFILES
 
