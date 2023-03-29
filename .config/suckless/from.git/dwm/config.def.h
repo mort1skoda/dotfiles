755573b23e90a832f4ece2cdeafa5418374a80f1
mort1skoda <mort1skoda@gmail.com>
2023-03-30 00:18:39 +0200

    [2023-03-30 00:18:38]

--- .config/suckless/from.git/dwm/config.def.h
+++ .config/suckless/from.git/dwm/config.def.h
@@ -5,7 +5,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
+//fontname=UbuntuMono Nerd Font Mono 11
+static const char *fonts[]          = { "UbuntuMono Nerd Font Mono:size=14" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
