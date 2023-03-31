052e6ca87aeae867c57ca987c4e986b90a9e8404
mort1skoda <mort1skoda@gmail.com>
2023-03-31 21:59:39 +0200

    sourcing: ~/.bash_aliases [2023-03-31 21:59:39] sunrise: sunset: +[%Y-%m-%d %H:%M:%S]

--- tip/vim.tip.sh
+++ tip/vim.tip.sh
@@ -3,8 +3,8 @@
 _BCL_R=$(cat $DOTFILES/tip/vim.lst |shuf -n1)
 if [[ $_BCL_R != "-" ]]
 then
-    printf "$col_blue"
-    printf "Vim tip......:"
+    printf "$col_cyan"
+    printf "Vim tip......: "
     echo $_BCL_R
     printf "$col_00"
 fi
