3cb9a956879e09fe0c9579e32fd6a10522b6b9b5
mort1skoda <mort1skoda@gmail.com>
2023-03-31 22:02:36 +0200

    sourcing: ~/.bash_aliases [2023-03-31 22:02:35] sunrise: sunset: +[%Y-%m-%d %H:%M:%S]

--- tip/vixen.tip.sh
+++ tip/vixen.tip.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-_BCL_R=$(cat $DOTFILES/tip/vixen.list |shuf -n1)
+_BCL_R=$(cat $DOTFILES/tip/vixen.lst |shuf -n1)
 if [[ $_BCL_R != "-" ]]
 then
-    printf "$col_blue"
-    printf "Vixen tip....:"
+    printf "$col_cyan"
+    printf "Vixen tip....: "
     echo $_BCL_R
     printf "$col_00"
 fi
