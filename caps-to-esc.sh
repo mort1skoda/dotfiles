ddb97b5b44095379874475ff5951ab2356efb5f9
mort1skoda <mort1skoda@gmail.com>
2023-03-31 11:02:07 +0200

    sourcing: ~/.bash_aliases [2023-03-31 11:02:06] sunrise: sunset: +[%Y-%m-%d %H:%M:%S]

---
+++ caps-to-esc.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Hello from test.sh"
+sleep 3
+echo "That was 5 sec. pause!"
+setxkbmap -option "caps:swapescape"
+sleep 3
+
+#xcape -e "Caps_Lock=Control_L|B"
+
+#xcape -e "Escape=Control_L|B;Caps_Lock=Escape"
+
+#
+#setxkbmap -option caps:escape
+#xcape -e 'Caps_Lock=Control_L|B'
+
+
