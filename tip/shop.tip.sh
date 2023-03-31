531223dba8c9c19514c743faec9a3c3b70367d04
mort1skoda <mort1skoda@gmail.com>
2023-03-31 21:58:39 +0200

    sourcing: ~/.bash_aliases [2023-03-31 21:58:38] sunrise: sunset: +[%Y-%m-%d %H:%M:%S]

--- tip/shop.tip.sh
+++ tip/shop.tip.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-_BCL_R=$(cat $DOTFILES/tip/shop.list |shuf -n1)
+_BCL_R=$(cat $DOTFILES/tip/shop.lst |shuf -n1)
 if [[ $_BCL_R != "-" ]]
 then
     printf "$col_cyan"
-    printf "Product tip..:"
+    printf "Product tip..: "
     echo $_BCL_R
     printf "$col_00"
 fi
