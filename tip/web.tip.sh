41dd8b511b10507df7700d73456bf3e5750017e6
mort1skoda <mort1skoda@gmail.com>
2023-03-31 22:05:39 +0200

    sourcing: ~/.bash_aliases [2023-03-31 22:05:38] sunrise: sunset: +[%Y-%m-%d %H:%M:%S]

--- tip/web.tip.sh
+++ tip/web.tip.sh
@@ -4,7 +4,7 @@ _BCL_R=$(cat $DOTFILES/tip/web.lst |shuf -n1)
 if [[ $_BCL_R != "-" ]]
 then
     printf "$col_cyan"
-    printf "Web tip......:"
+    printf "Web tip......: "
     echo $_BCL_R
     printf "$col_00"
 fi
