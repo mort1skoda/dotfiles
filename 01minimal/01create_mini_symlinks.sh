912797cd1daf2b672c2b6f83f6bc26cbbdfbbce1
mort1skoda <mort1skoda@gmail.com>
2023-04-10 12:29:43 +0200

    df

--- 01minimal/01create_mini_symlinks.sh
+++ 01minimal/01create_mini_symlinks.sh
@@ -2,5 +2,4 @@ ln -svf /dat.mnt/dotfiles/01minimal/.bash_profile  /home/m/.bash_profile
 ln -svf /dat.mnt/dotfiles/01minimal/.bashrc        /home/m/.bashrc
 ln -svf /dat.mnt/dotfiles/01minimal/.config        /home/m/.config
 ln -svf /dat.mnt/dotfiles/01minimal/.vimrc         /home/m/.vimrc
-ln -svf /dat.mnt/dotfiles/01minimal/.vifm          /home/m/.vifm
 ls -al --color /home/m
