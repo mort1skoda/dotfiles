5b7e296a0963090e3ad7179ad30f38af12f8c821
mort1skoda <mort1skoda@gmail.com>
2023-04-10 19:07:05 +0200

    upd

--- 01minimal/01create_mini_symlinks.sh
+++ 01minimal/01create_mini_symlinks.sh
@@ -4,4 +4,5 @@ ln -svf /dat.mnt/dotfiles/01minimal/.bashrc        /home/m/.bashrc
 rm -vrf ~/.config
 ln -svf /dat.mnt/dotfiles/01minimal/.config        /home/m/.config
 ln -svf /dat.mnt/dotfiles/01minimal/.vimrc         /home/m/.vimrc
+ln -svf /dat.mnt/dotfiles/01minimal/.tmux.conf     /home/m/.tmux.conf
 ls -al --color /home/m
