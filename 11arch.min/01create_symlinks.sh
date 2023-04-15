ff9fc1e0906b76342761294a6e9424b47361185f
mort1skoda <mort1skoda@gmail.com>
2023-04-15 09:09:05 +0200

    upd

--- 11arch.min/01create_symlinks.sh
+++ 11arch.min/01create_symlinks.sh
@@ -1,25 +1,37 @@
+#     To substitute new for the first old in a line type    :s/old/new
+#     To substitute new for all 'old's on a line type	   :s/old/new/g
+#     To substitute phrases between two line #'s type	   :#,#s/old/new/g
+#     To substitute all occurrences in the file type	   :%s/old/new/g
+#     To ask for confirmation each time add 'c'		   :%s/old/new/gc
+#  to insert char at first column: ctrl-v, mark down lines to insert char,
+#  type I (capital i), then esc, all lines will get new char at first column.
 
-ln -svf $PWD/.bash_profile  ~/.bash_profile
+
+rm -vrf ~/.bash_profile  
+ln -svf /dat.mnt/dotfiles/11arch.min/.bash_profile  ~/.bash_profile
 #exit
 
-ln -svf $PWD/.bashrc        ~/.bashrc
+rm -vrf ~/.bashrc
+ln -svf /dat.mnt/dotfiles/11arch.min/.bashrc        ~/.bashrc
 #exit
 
 rm -vrf ~/.config
-ln -svf $PWD/.config        ~/.config
+ln -svf /dat.mnt/dotfiles/11arch.min/.config        ~/.config
 #exit
 
 rm -vrf ~/.vimrc
-ln -svf $PWD/.vimrc         ~/.vimrc
+ln -svf /dat.mnt/dotfiles/11arch.min/.vimrc         ~/.vimrc
 #exit
 
 rm -vrf ~/.vim
-ln -svf $PWD/.vim           ~/.vim
+ln -svf /dat.mnt/dotfiles/11arch.min/.vim           ~/.vim
 #exit
 
-ln -svf $PWD/.tmux.conf     ~/.tmux.conf
+rm -vrf ~/.tmux.conf
+ln -svf /dat.mnt/dotfiles/11arch.min/.tmux.conf     ~/.tmux.conf
+#exit
 
 ls -al --color ~
 
-./02copy_files.sh
+#./02copy_files.sh
 
